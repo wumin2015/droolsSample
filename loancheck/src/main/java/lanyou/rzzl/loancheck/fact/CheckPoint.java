a819a93ab3bfcf7d0a0f032cafd01ed94e4755ee
tomcat <>
2018-01-06 16:27:37 +0800

    {/loancheck/src/main/java/lanyou/rzzl/loancheck/fact/CheckPoint.java}

--- loancheck/src/main/java/lanyou/rzzl/loancheck/fact/CheckPoint.java
+++ loancheck/src/main/java/lanyou/rzzl/loancheck/fact/CheckPoint.java
@@ -8,6 +8,7 @@ public class CheckPoint implements java.io.Serializable {
 
 	static final long serialVersionUID = 1L;
 
+	@org.kie.api.definition.type.Description("年龄")
 	private java.lang.Integer age;
 	private java.lang.Double income;
 	private java.lang.Boolean hasOtherLoan;
